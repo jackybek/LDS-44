857a49ed80695bc85011f24c19b72599cffc1162
jackybek <jackybek@gmail.com>
2023-12-30 20:33:46 +0800

    Update compiledocker-44.sh

--- compiledocker-44.sh
+++ compiledocker-44.sh
@@ -1,4 +1,4 @@
 sudo ./killdocker-44.sh
-sudo docker build -f dockerfile-44 -t opcuaserver44:v1.0 --tag 'open62541' .
-sudo docker run -it -d --name opcuaserver44 opcuaserver44 bash
-sudo docker exec -it opcuaserver44 /bin/bash
+sudo docker build -f dockerfile-44 -t opcualdsserver44:v1.0 --tag 'lds-44' .
+sudo docker run -it -d --name opcualdsserver44 opcualdsserver44 bash
+sudo docker exec -it opcualdsserver44 /bin/bash
