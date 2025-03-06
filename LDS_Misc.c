79727d69f88119510bd9bba1832912621b5a7b49
jackybek <jackybek@gmail.com>
2025-03-06 11:46:16 +0800

    Update LDS_Misc.c

--- LDS_Misc.c
+++ LDS_Misc.c
@@ -23,7 +23,6 @@
 #include <open62541/server_config_default.h>
 
 UA_ByteString loadFile(const char *const);
-static void serverOnNetworkCallback(const UA_ServerOnNetwork *, UA_Boolean, UA_Boolean, void *);
 
 UA_ByteString loadFile(const char *const path)
 {
