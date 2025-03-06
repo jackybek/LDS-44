cd33e8dd396d986bb1fb56a7202490996ce09fe5
jackybek <jackybek@gmail.com>
2025-03-06 10:57:38 +0800

    Update LDS_Misc.c

--- LDS_Misc.c
+++ LDS_Misc.c
@@ -9,6 +9,17 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <pthread.h>
+
+//#include <open62541/plugin/network.h>
+#include <open62541/client_highlevel.h>
+#include <open62541/plugin/log_stdout.h>
+#include <open62541/client_config_default.h>
+#include <open62541/plugin/create_certificate.h>
+#include <open62541/plugin/securitypolicy.h>
+#include <open62541/plugin/accesscontrol_default.h>
+#include <open62541/plugin/nodestore_default.h>
+//#include <open62541/plugin/pki_default.h>
+#include <open62541/server.h>
 #include <open62541/server_config_default.h>
 
 UA_ByteString loadFile(const char *const);
